e5c5f6bedef6c30505b966159abc3e0e0d705f7a
Maksim <danialvalen@gmail.com>
2026-09-09 16:11:07 +0300

    add probel

--- file2.py
+++ file2.py
@@ -91,7 +91,7 @@ class Calculator:
 def main():
     calculator = Calculator()
     calculator.run()
-    print("\nСпасибо большое.")
+    print("\nСпасибо большое .")
 
 if __name__ == "__main__":
     main()
